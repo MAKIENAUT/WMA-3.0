d1dff4d7037d49bf75d4b37f0e7f0d2de4fd54b7
emzLangOk <emzkey09@gmail.com>
2023-10-05 08:44:29 +0800

    Added contents of Temporary Employment

---
+++ Pages/Process/temporary-employment.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+   <!-- FontAwesome -->
+   <link rel="stylesheet" crossorigin="anonymous" referrerpolicy="no-referrer"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+      integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" />
+   <!-- FontAwesome -->
+
+   <link rel="stylesheet" href="temporary-employment.css" />
+   <link rel="stylesheet" href="/Pages/Global//global.css" />
+   <link rel="icon" type="image/x-icon" href="/Photos/WMA.png">
+   <script defer src="/Pages/Global/global.js"></script>
+   <title>WMA Home</title>
+</head>
+
+<body>
+   <!-- NAV SECTION -->
+   <nav class="navbar-parent">
+      <div class="navbar-container">
+         <div class="navbar-main-container">
+            <a class="navbar-logo" href="#">
+               <img src="/Photos/wma-logo.png" alt="WMA Logo" />
+            </a>
+            <button id="navbarMobileIcon" class="navbar-mobile-icon fa fa-bars burger-icon" onclick="toggleNavbar()">
+            </button>
+         </div>
+         <div id="navbarLinkContainer" class="navbar-link-container">
+            <a class="navbar-link" href="#">News</a>
+            <div class="navbar-category-container" href="javascript:void(0)">
+               <p id="navbarCategoryContainer" onclick="toggleCategory()">Process<i class="fa-solid fa-caret-down"></i>
+               </p>
+               <div id="navbarCategory" class="navbar-category">
+                  <a class="navbar-category-link" href="#">Family Based</a>
+                  <a class="navbar-category-link" href="#">Study and Exchange</a>
+                  <a class="navbar-category-link" href="#">Temporary Employment</a>
+               </div>
+            </div>
+            <div class="navbar-category-container" href="javascript:void(0)">
+               <p id="navbarCategoryContainer2" onclick="toggleCategory2()">Categories<i
+                     class="fa-solid fa-caret-down"></i></p>
+               <div id="navbarCategory2" class="navbar-category">
+                  <a class="navbar-category-link" href="#">Family Based</a>
+                  <a class="navbar-category-link" href="#">Study and Exchange (J-1)</a>
+                  <a class="navbar-category-link" href="#">Temporary Employment (EB-3/H2A)</a>
+                  <a class="navbar-category-link" href="#">Other Uploads</a>
+               </div>
+            </div>
+            <a class="navbar-link" href="#">About Us</a>
+            <a class="navbar-link" href="#">Login/Sign up</a>
+         </div>
+      </div>
+   </nav>
+
+   <!-- MAIN SECTION -->
+   <main>
+      <!-- PROCESS HEADER SECTION -->
+      <header class="header-parent">
+         <div class="header-container">
+            <div class="header-content">
+               <h1>Process of H2A</h1>
+            </div>
+         </div>
+      </header>
+
+      <!-- PROCESS STEPS SECTION -->
+      <article class="steps-parent">
+         <div class="steps-container">
+            <div class="steps-content">
+               <div class="steps-header">
+                  <p>How to Apply?</p>
+                  <h1>FOLLOW THESE STEPS!</h1>
+                  <p>The H-2A visa is a temporary work visa for foreign agricultural workers who will perform seasonal or temporary agricultural work in the United States. Here is an overview of the general process of obtaining an H-2A visa:</p>
+               </div>
+               <div class="steps-description">
+                  <div class="steps-card">
+                     <div class="steps-card-header">
+                        <h1>01</h1>
+                        <h1>EMPLOYER RECRUITMENT</h1>
+                     </div>
+                     <p>The U.S. employer must first demonstrate that there are not enough U.S. workers who are able, willing, qualified, and available to do the temporary work, and that hiring foreign workers will not adversely affect the wages and working conditions of U.S. workers similarly employed.</p>
+                  </div>
+                  <div class="steps-card">
+                     <div class="steps-card-header">
+                        <h1>02</h1>
+                        <h1>FILE A LABOR CERTIFICATION APPLICATION</h1>
+                     </div>
+                     <p>The U.S. employer must file a labor certification application with the U.S. Department of Labor (DOL) to demonstrate that they have met the recruitment requirements and that the job offer meets the minimum requirements set by the DOL.</p>
+                  </div>
+                  <div class="steps-card">
+                     <div class="steps-card-header">
+                        <h1>03</h1>
+                        <h1>SUBMIT FORM I-129</h1>
+                     </div>
+                     <p>The U.S. employer must file a Form I-129, Petition for Nonimmigrant Worker, with U.S. Citizenship and Immigration Services (USCIS), along with supporting documentation and the approved labor certification.</p>
+                  </div>
+                  <div class="steps-card">
+                     <div class="steps-card-header">
+                        <h1>04</h1>
+                        <h1>CONSULAR PROCESSING</h1>
+                     </div>
+                     <p>Once the petition is approved, the foreign worker must apply for a visa at a U.S. consulate or embassy abroad.</p>
+                  </div>
+                  <div class="steps-card">
+                     <div class="steps-card-header">
+                        <h1>05</h1>
+                        <h1>ADMISSIBILITY</h1>
+                     </div>
+                     <p>The foreign worker must demonstrate that they are admissible to the United States and not subject to any grounds of inadmissibility such as criminal history, health concerns, or prior immigration violations.</p>
+                  </div>
+                  <div class="steps-card">
+                     <div class="steps-card-header">
+                        <h1>06</h1>
+                        <h1>ARRIVAL IN THE UNITED STATES</h1>
+                     </div>
+                     <p>If the visa is approved, the foreign worker can enter the United States and begin work for the U.S. employer.</p>
+                  </div>
+               </div>
+               <div class="steps-conclusion">
+                     <p>It is important to note that the H-2A visa process can be complex, and it is highly recommended that employers seek the assistance of an experienced immigration attorney to ensure compliance with all legal requirements.</p>
+                     
+               </div>
+               <div class="link">
+                  <a href="#">Learn More</a>
+               </div>
+            </div>
+         </div>
+         </div>
+      </article>
+   </main>
+
+   <!-- FOOTER SECTION -->
+   <footer class="footer-parent">
+      <section class="footer-content">
+         <section class="footer-main-content">
+            <section class="footer-disclaimer">
+               <div class="disclaimer-content">
+                  <img src="/Photos/wma-logo.png" alt="WMA logo">
+                  <p>West Migration Agency LLC (“WMA”) is the parent company of West Migration Consultancy Inc.,(“WMC”)
+                     based
+                     in
+                     the Philippines. WMC and its state affiliates advance the corporation's interest to engage in
+                     immigration
+                     consultancy by providing expert advice to prospective clients for the USA through qualification
+                     assistance,
+                     processing of applications, and other related documents. </p>
+               </div>
+               <div class="misc-photos">
+                  <img src="/Photos/wmc-logo.png" alt="WMC logo">
+                  <img src="/Photos/USCIS.svg" alt="USCIS logo">
+               </div>
+            </section>
+            <section class="footer-info">
+               <section class="footer-contact">
+                  <h1>Contact us</h1>
+                  <div class="contact-content">
+                     <div class="contact-address">
+                        <header><i class="fa-solid fa-location-dot"></i> Address:</header>
+                        <div>3400 Cottage Way STE. G2 #11495 Sacramento, CA 95825</div>
+                     </div>
+                     <div class="contact-email">
+                        <header><i class="fa-solid fa-envelope"></i> Email:</header>
+                        <div>admin@westmigrationagency.us</div>
+                     </div>
+                     <div class="contact-number">
+                        <header><i class="fa-solid fa-phone"></i> Phone:</header>
+                        <div>415-633-6865</div>
+                     </div>
+                  </div>
+               </section>
+               <section class="footer-ph-office">
+                  <h1>PH Office</h1>
+                  <div class="contact-content">
+                     <div class="contact-address">
+                        <header><i class="fa-solid fa-location-dot"></i> Address (Cebu Branch):</header>
+                        <div>11th Floor, AppleOne - Equicom Tower Mindanao Avenue, cor. Biliran St., Cebu Business Park,
+                           Cebu
+                           City 6000 Philippines</div>
+                     </div>
+                     <div class="contact-address">
+                        <header><i class="fa-solid fa-location-dot"></i> Address (Dagupan Branch):</header>
+                        <div>Upcoming!</div>
+                     </div>
+                  </div>
+               </section>
+            </section>
+         </section>
+         <section class="footer-more-info">
+            <section class="about-us">
+               <h1>About Us</h1>
+               <div class="footer-more-info-content">
+                  <a href="#">About us</a>
+                  <a href="#">Specialization</a>
+               </div>
+            </section>
+            <section class="footer-process">
+               <h1>Process</h1>
+               <div class="footer-more-info-content">
+                  <a href="#">Family Based</a>
+                  <a href="#">Study and Exchange</a>
+                  <a href="#">Temporary Employment</a>
+               </div>
+            </section>
+            <section class="footer-links">
+               <h1>More Info</h1>
+               <div class="footer-more-info-content">
+                  <a href="#">News</a>
+                  <a href="#">Contact us</a>
+                  <a href="#">Administrator</a>
+               </div>
+            </section>
+            <section class="footer-socials">
+               <h1>Our Social Links</h1>
+               <div class="social-content">
+                  <a href="#" class="fa-brands fa-facebook"></a>
+                  <a href="#" class="fa-brands fa-instagram"></a>
+               </div>
+            </section>
+         </section>
+      </section>
+      <section class="copyright">
+         <p>Copyright 2023</p>
+      </section>
+   </footer>
+</body>
+
+</html>
