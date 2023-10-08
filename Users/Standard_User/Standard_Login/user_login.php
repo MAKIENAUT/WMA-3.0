ee9640dba885d8718eea8bebf5ac287895bb80c9
vsly <101377492+Noxaelum@users.noreply.github.com>
2023-10-08 16:03:47 +0800

    Finished basic design for login

--- Users/Standard_User/Standard_Login/user_login.php
+++ Users/Standard_User/Standard_Login/user_login.php
@@ -85,11 +85,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="user_login.css">
-   <script src="user_login.js"></script>
+   <script defer src="user_login.js"></script>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
       integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
       crossorigin="anonymous" referrerpolicy="no-referrer">
-   <title>Document</title>
+   <link rel="stylesheet" href="/Pages/Global/global.css" />
+   <link rel="icon" type="image/x-icon" href="/Photos/WMA.png">
+   <title>WMA Login</title>
 </head>
 
 <body>
