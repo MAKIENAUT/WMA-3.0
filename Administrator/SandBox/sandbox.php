5d471b58b023e76860981a4587e10e25d1645048
MAKIENAUT <mcrayescoto@gmail.com>
2023-10-14 23:21:25 +0800

    PUSHED: ADMIN REHABILITATION

---
+++ Administrator/SandBox/sandbox.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Scripts</title>
+</head>
+
+<body>
+
+    <?php
+    include 'script1.php';
+    include 'script2.php';
+
+    echo $script_1;
+
+
+    ?>
+
+</body>
+
+</html>
