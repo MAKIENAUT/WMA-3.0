f494bf46ee79908c25ceea8229626b072449e936
vsly <101377492+Noxaelum@users.noreply.github.com>
2023-10-18 13:31:26 +0800

    Restructuring of code for user-side

---
+++ Pages/Global/footer.php
@@ -0,0 +1,92 @@
+<footer class="footer-parent">
+   <section class="footer-content">
+      <section class="footer-main-content">
+         <section class="footer-disclaimer">
+            <div class="disclaimer-content">
+               <img src="/Photos/wma-logo.png" alt="WMA logo">
+               <p>West Migration Agency LLC (“WMA”) is the parent company of West Migration Consultancy Inc.,(“WMC”)
+                  based
+                  in
+                  the Philippines. WMC and its state affiliates advance the corporation's interest to engage in
+                  immigration
+                  consultancy by providing expert advice to prospective clients for the USA through qualification
+                  assistance,
+                  processing of applications, and other related documents. </p>
+            </div>
+            <div class="misc-photos">
+               <img src="/Photos/wmc-logo.png" alt="WMC logo">
+               <img src="/Photos/USCIS.svg" alt="USCIS logo">
+            </div>
+         </section>
+         <section class="footer-info">
+            <section class="footer-contact">
+               <h1>Contact us</h1>
+               <div class="contact-content">
+                  <div class="contact-address">
+                     <header><i class="fa-solid fa-location-dot"></i> Address:</header>
+                     <div>3400 Cottage Way STE. G2 #11495 Sacramento, CA 95825</div>
+                  </div>
+                  <div class="contact-email">
+                     <header><i class="fa-solid fa-envelope"></i> Email:</header>
+                     <div>admin@westmigrationagency.us</div>
+                  </div>
+                  <div class="contact-number">
+                     <header><i class="fa-solid fa-phone"></i> Phone:</header>
+                     <div>415-633-6865</div>
+                  </div>
+               </div>
+            </section>
+            <section class="footer-ph-office">
+               <h1>PH Office</h1>
+               <div class="contact-content">
+                  <div class="contact-address">
+                     <header><i class="fa-solid fa-location-dot"></i> Address (Cebu Branch):</header>
+                     <div>11th Floor, AppleOne - Equicom Tower Mindanao Avenue, cor. Biliran St., Cebu Business Park,
+                        Cebu
+                        City 6000 Philippines</div>
+                  </div>
+                  <div class="contact-address">
+                     <header><i class="fa-solid fa-location-dot"></i> Address (Dagupan Branch):</header>
+                     <div>Upcoming!</div>
+                  </div>
+               </div>
+            </section>
+         </section>
+      </section>
+      <section class="footer-more-info">
+         <section class="about-us">
+            <h1>About Us</h1>
+            <div class="footer-more-info-content">
+               <a href="/Pages/About-us/about.php">About us</a>
+               <a href="#">Specialization</a>
+            </div>
+         </section>
+         <section class="footer-process">
+            <h1>Process</h1>
+            <div class="footer-more-info-content">
+               <a href="/Pages/Process/family-based.php">Family Based</a>
+               <a href="/Pages/Process/study-and-exchange.php">Study and Exchange</a>
+               <a href="/Pages/Process/temporary-employment.php">Temporary Employment</a>
+            </div>
+         </section>
+         <section class="footer-links">
+            <h1>More Info</h1>
+            <div class="footer-more-info-content">
+               <a href="#">News</a>
+               <a href="#">Contact us</a>
+               <a href="#">Administrator</a>
+            </div>
+         </section>
+         <section class="footer-socials">
+            <h1>Our Social Links</h1>
+            <div class="social-content">
+               <a href="https://www.facebook.com/WestMigration" class="fa-brands fa-facebook"></a>
+               <a href="#" class="fa-brands fa-instagram"></a>
+            </div>
+         </section>
+      </section>
+   </section>
+   <section class="copyright">
+      <p>Copyright 2023</p>
+   </section>
+</footer>
--- Pages/Process/family-based.php
+++ Pages/Process/family-based.php
@@ -20,41 +20,9 @@
 
 <body>
    <!-- NAV SECTION -->
-   <nav class="navbar-parent">
-      <div class="navbar-container">
-         <div class="navbar-main-container">
-            <a class="navbar-logo" href="/Pages/Home/home.php">
-               <img src="/Photos/wma-logo.png" alt="WMA Logo" />
-            </a>
-            <button id="navbarMobileIcon" class="navbar-mobile-icon fa fa-bars burger-icon" onclick="toggleNavbar()">
-            </button>
-         </div>
-         <div id="navbarLinkContainer" class="navbar-link-container">
-            <a class="navbar-link" href="#">News</a>
-            <div class="navbar-category-container" href="javascript:void(0)">
-               <p id="navbarCategoryContainer" onclick="toggleCategory()">Process<i class="fa-solid fa-caret-down"></i>
-               </p>
-               <div id="navbarCategory" class="navbar-category">
-                  <a class="navbar-category-link" href="/Pages/Process/family-based.php">Family Based</a>
-                  <a class="navbar-category-link" href="/Pages/Process/study-and-exchange.php">Study and Exchange</a>
-                  <a class="navbar-category-link" href="/Pages/Process/temporary-employment.php">Temporary
-                     Employment</a>
-               </div>
-            </div>
-            <div class="navbar-category-container" href="javascript:void(0)">
-               <p id="navbarCategoryContainer2" onclick="toggleCategory2()">Categories<i
-                     class="fa-solid fa-caret-down"></i></p>
-               <div id="navbarCategory2" class="navbar-category">
-                  <a class="navbar-category-link" href="#">Family Based</a>
-                  <a class="navbar-category-link" href="#">Study and Exchange (J-1)</a>
-                  <a class="navbar-category-link" href="#">Temporary Employment (EB-3/H2A)</a>
-               </div>
-            </div>
-            <a class="navbar-link" href="/Pages/About-us/about.php">About Us</a>
-            <a class="navbar-link" href="#">Login/Sign up</a>
-         </div>
-      </div>
-   </nav>
+   <?php
+   require_once "../Global/navbar.php";
+   ?>
 
    <!-- MAIN SECTION -->
    <main>
@@ -155,98 +123,9 @@
    </main>
 
    <!-- FOOTER SECTION -->
-   <footer class="footer-parent">
-      <section class="footer-content">
-         <section class="footer-main-content">
-            <section class="footer-disclaimer">
-               <div class="disclaimer-content">
-                  <img src="/Photos/wma-logo.png" alt="WMA logo">
-                  <p>West Migration Agency LLC (“WMA”) is the parent company of West Migration Consultancy Inc.,(“WMC”)
-                     based
-                     in
-                     the Philippines. WMC and its state affiliates advance the corporation's interest to engage in
-                     immigration
-                     consultancy by providing expert advice to prospective clients for the USA through qualification
-                     assistance,
-                     processing of applications, and other related documents. </p>
-               </div>
-               <div class="misc-photos">
-                  <img src="/Photos/wmc-logo.png" alt="WMC logo">
-                  <img src="/Photos/USCIS.svg" alt="USCIS logo">
-               </div>
-            </section>
-            <section class="footer-info">
-               <section class="footer-contact">
-                  <h1>Contact us</h1>
-                  <div class="contact-content">
-                     <div class="contact-address">
-                        <header><i class="fa-solid fa-location-dot"></i> Address:</header>
-                        <div>3400 Cottage Way STE. G2 #11495 Sacramento, CA 95825</div>
-                     </div>
-                     <div class="contact-email">
-                        <header><i class="fa-solid fa-envelope"></i> Email:</header>
-                        <div>admin@westmigrationagency.us</div>
-                     </div>
-                     <div class="contact-number">
-                        <header><i class="fa-solid fa-phone"></i> Phone:</header>
-                        <div>415-633-6865</div>
-                     </div>
-                  </div>
-               </section>
-               <section class="footer-ph-office">
-                  <h1>PH Office</h1>
-                  <div class="contact-content">
-                     <div class="contact-address">
-                        <header><i class="fa-solid fa-location-dot"></i> Address (Cebu Branch):</header>
-                        <div>11th Floor, AppleOne - Equicom Tower Mindanao Avenue, cor. Biliran St., Cebu Business Park,
-                           Cebu
-                           City 6000 Philippines</div>
-                     </div>
-                     <div class="contact-address">
-                        <header><i class="fa-solid fa-location-dot"></i> Address (Dagupan Branch):</header>
-                        <div>Upcoming!</div>
-                     </div>
-                  </div>
-               </section>
-            </section>
-         </section>
-         <section class="footer-more-info">
-            <section class="about-us">
-               <h1>About Us</h1>
-               <div class="footer-more-info-content">
-                  <a href="/Pages/About-us/about.php">About us</a>
-                  <a href="#">Specialization</a>
-               </div>
-            </section>
-            <section class="footer-process">
-               <h1>Process</h1>
-               <div class="footer-more-info-content">
-                  <a href="/Pages/Process/family-based.php">Family Based</a>
-                  <a href="/Pages/Process/study-and-exchange.php">Study and Exchange</a>
-                  <a href="/Pages/Process/temporary-employment.php">Temporary Employment</a>
-               </div>
-            </section>
-            <section class="footer-links">
-               <h1>More Info</h1>
-               <div class="footer-more-info-content">
-                  <a href="#">News</a>
-                  <a href="#">Contact us</a>
-                  <a href="#">Administrator</a>
-               </div>
-            </section>
-            <section class="footer-socials">
-               <h1>Our Social Links</h1>
-               <div class="social-content">
-                  <a href="https://www.facebook.com/WestMigration" class="fa-brands fa-facebook"></a>
-                  <a href="#" class="fa-brands fa-instagram"></a>
-               </div>
-            </section>
-         </section>
-      </section>
-      <section class="copyright">
-         <p>Copyright 2023</p>
-      </section>
-   </footer>
+   <?php
+   require_once "../Global/footer.php";
+   ?>
 </body>
 
 </html>
